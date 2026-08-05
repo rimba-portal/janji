06a1e333cd6685901e37d11f5eff127617510922
Faros <114036707+a-faros@users.noreply.github.com>
2026-08-05 12:48:20 +0800

    Release v1.2.6

--- src/AgreementServiceProvider.php
+++ src/AgreementServiceProvider.php
@@ -6,16 +6,18 @@ namespace Rimba\Agreement;
 
 use Rimba\Base\Services\BitesServiceProvider;
 
+
 class AgreementServiceProvider extends BitesServiceProvider
 {
+
     protected function bootPackage(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
         //
     }
-
     protected function registerPackage(): void
     {
         //
     }
+
 }
